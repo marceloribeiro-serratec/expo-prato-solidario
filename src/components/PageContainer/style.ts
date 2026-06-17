f4c8f94b2a2d672674aea5906509d8452815d0a2
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-17 17:58:14 -0300

    feat: update PageContainer style to use COLORS constant for background color

--- src/components/PageContainer/style.ts
+++ src/components/PageContainer/style.ts
@@ -1,10 +1,11 @@
+import { COLORS } from '@/constants/colors';
 import { StyleSheet } from 'react-native';
 
 export const pageContainer = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#000',
+        backgroundColor: COLORS.white,
         paddingHorizontal: 12,
         paddingTop: 24,
         paddingBottom: 24,
