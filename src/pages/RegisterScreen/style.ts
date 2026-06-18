79624a949c3cabd227781a6887b9820ebd76a2e6
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-18 15:19:35 -0300

    feat: implement RegisterScreen layout and styles; add LoginForm component and update navigation

---
+++ src/pages/RegisterScreen/style.ts
@@ -0,0 +1,62 @@
+import { COLORS } from "@/constants";
+import { StyleSheet } from "react-native";
+
+export const registerScreen = StyleSheet.create({
+    container: {
+        paddingVertical: 42,
+        paddingHorizontal: 24,
+        justifyContent: "center",
+        flex: 1,
+    },
+    gradient: {
+        flex: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    logoContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 24,
+    },
+    titleContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    formContainer: {
+        marginTop: 42,
+        backgroundColor: COLORS.white,
+        padding: 24,
+        borderRadius: 8,
+        shadowColor: COLORS.black,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4.65,
+        elevation: 8,
+    },
+    subtitleContainer: {
+        marginTop: 54,
+    },
+    signupContainer: {
+        marginTop: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        gap: 4,
+    },
+    signupText: {
+        width: "auto",
+        paddingTop: 0,
+        lineHeight: 22,
+    },
+    signupButton: {
+        height: "auto",
+        paddingVertical: 0,
+        paddingHorizontal: 0,
+    },
+});
