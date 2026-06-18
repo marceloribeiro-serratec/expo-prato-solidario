ab63a7c9709c8ec2d10d614015b3400e93ffe434
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-18 07:04:09 -0300

    feat: add expo-blur and update WelcomeScreen with BlurView and LinearGradient

--- src/pages/WelcomeScreen/index.tsx
+++ src/pages/WelcomeScreen/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { BlurView } from "expo-blur";
 
-import { PageContainerImage } from '@/components/PageContainerImage';
+import { PageContainerImage } from "@/components/PageContainerImage";
+
+import { welcomeScreen } from "./style";
 
 export function WelcomeScreen() {
     return (
         <PageContainerImage>
-            <Text>WelcomeScreen</Text>
+            <BlurView
+                intensity={20}
+                tint="default"
+                experimentalBlurMethod="dimezisBlurView"
+                style={welcomeScreen.blur}
+            />
+            <LinearGradient
+                colors={["rgba(255,255,255,0.7)", "rgba(0,0,0,0.3)","rgba(0,0,0,0.9)"]}
+                style={welcomeScreen.gradient}
+            >
+            </LinearGradient>
         </PageContainerImage>
     );
 }
---
+++ src/pages/WelcomeScreen/style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const welcomeScreen = StyleSheet.create({
+    blur: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    gradient:{
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+});
