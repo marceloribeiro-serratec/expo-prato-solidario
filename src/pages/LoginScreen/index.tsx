1c992a0bd2c031c2aec22e408e9981a7bd7dbf59
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-18 18:39:23 -0300

    feat: update statusBarStyle to 'dark' in LoginScreen and RegisterScreen; change initial route to 'welcome' in StackRoutes

--- src/pages/LoginScreen/index.tsx
+++ src/pages/LoginScreen/index.tsx
@@ -18,7 +18,7 @@ export function LoginScreen() {
     const navigation = useNavigation<NavigationProps>();
 
     return (
-        <PageContainerImage statusBarStyle="light" statusBarBackgroundColor={COLORS.black}>
+        <PageContainerImage statusBarStyle="dark" statusBarBackgroundColor={COLORS.black}>
             <LinearGradient
                 colors={[
                     "rgba(255,255,255,0.9)",
--- src/pages/RegisterScreen/index.tsx
+++ src/pages/RegisterScreen/index.tsx
@@ -18,7 +18,7 @@ export function RegisterScreen() {
     const navigation = useNavigation<NavigationProps>();
 
     return (
-        <PageContainerScroll statusBarStyle="light" statusBarBackgroundColor={COLORS.black}>
+        <PageContainerScroll statusBarStyle="dark" statusBarBackgroundColor={COLORS.black}>
             <LinearGradient
                 colors={[
                     "rgba(255,255,255,0.9)",
