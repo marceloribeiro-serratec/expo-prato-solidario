2eb8a8a875a4deee62d3648a1e8fef0ec08ddf47
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-18 16:56:35 -0300

    feat: enhance RegisterForm with improved styling and input components; add validation schema

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -1,11 +1,16 @@
 import { View, Text, TextInput } from "react-native";
-import ButtonForm from "../LoginForm/button-form";
-import { globalStyles } from "../styles/globalStyles";
+
 import { Controller, useForm } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight } from "lucide-react-native";
+
 import { IconInput } from "../IconInput";
 import { useState } from "react";
+import { Button } from "../Button";
+import { Title } from "../Title";
+import { COLORS } from "@/constants";
+import { registerForm } from "./style";
 
 const registerFormSchema = z
     .object({
@@ -14,13 +19,18 @@ const registerFormSchema = z
             .nonempty("Nome é obrigatório")
             .min(3, "Nome deve ter pelo menos 3 caracteres")
             .regex(/^[a-zA-Z\s]+$/, "Nome deve conter apenas letras e espaços"),
-        email: z.string().nonempty("E-mail é obrigatório").email("E-mail inválido"),
+        email: z
+            .string()
+            .nonempty("E-mail é obrigatório")
+            .email("E-mail inválido"),
         senha: z
             .string()
             .min(8, "Senha deve ter pelo menos 8 caracteres")
             .max(20, "Senha deve ter no máximo 20 caracteres")
             .nonempty("Senha é obrigatória"),
-        confirmarSenha: z.string().nonempty("Confirmação de senha é obrigatória"),
+        confirmarSenha: z
+            .string()
+            .nonempty("Confirmação de senha é obrigatória"),
     })
     .refine((data) => data.senha === data.confirmarSenha, {
         message: "As senhas não coincidem",
@@ -64,147 +74,159 @@ export function RegisterForm() {
     return (
         <View>
             <View style={{ marginBottom: 8 }}>
-                <Text style={globalStyles.label}>Nome</Text>
+                <Text style={registerForm.label}>Nome</Text>
                 <Controller
                     control={control}
                     name="nome"
                     rules={{ required: true }}
                     render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Digite seu nome..."
-                            placeholderTextColor="#999"
-                            style={[
-                                globalStyles.input,
-                                { paddingLeft: 50, paddingRight: 10 },
-                            ]}
-                            keyboardType="default"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            onBlur={onBlur}
-                            onChangeText={(value) => onChange(value)}
-                            value={value}
-                        />
+                        <View style={registerForm.inputContainer}>
+                            <IconInput iconName="user" />
+                            <TextInput
+                                placeholder="Digite seu nome..."
+                                placeholderTextColor="#999"
+                                style={[
+                                    registerForm.input,
+                                    { paddingLeft: 50, paddingRight: 10 },
+                                ]}
+                                keyboardType="default"
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onBlur={onBlur}
+                                onChangeText={(value) => onChange(value)}
+                                value={value}
+                            />
+                        </View>
                     )}
                 />
-                <InputIcon iconName="user" />
 
                 {errors.nome && (
-                    <Text style={globalStyles.errorText}>
+                    <Text style={registerForm.errorText}>
                         {errors.nome.message}
                     </Text>
                 )}
             </View>
 
             <View style={{ marginBottom: 8 }}>
-                <Text style={globalStyles.label}>E-mail</Text>
+                <Text style={registerForm.label}>E-mail</Text>
                 <Controller
                     control={control}
                     name="email"
                     rules={{ required: true }}
                     render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Digite seu email..."
-                            placeholderTextColor="#999"
-                            style={[
-                                globalStyles.input,
-                                { paddingLeft: 50, paddingRight: 10 },
-                            ]}
-                            keyboardType="default"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            onBlur={onBlur}
-                            onChangeText={(value) => onChange(value)}
-                            value={value}
-                        />
+                        <View style={registerForm.inputContainer}>
+                            <IconInput iconName="mail" />
+                            <TextInput
+                                placeholder="Digite seu email..."
+                                placeholderTextColor="#999"
+                                style={[
+                                    registerForm.input,
+                                    { paddingLeft: 50, paddingRight: 10 },
+                                ]}
+                                keyboardType="default"
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onBlur={onBlur}
+                                onChangeText={(value) => onChange(value)}
+                                value={value}
+                            />
+                        </View>
                     )}
                 />
-                <InputIcon iconName="mail" />
 
                 {errors.email && (
-                    <Text style={globalStyles.errorText}>
+                    <Text style={registerForm.errorText}>
                         {errors.email.message}
                     </Text>
                 )}
             </View>
 
             <View style={{ marginBottom: 8 }}>
-                <Text style={globalStyles.label}>senha</Text>
+                <Text style={registerForm.label}>senha</Text>
                 <Controller
                     control={control}
                     name="senha"
                     rules={{ required: true }}
                     render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Digite sua senha..."
-                            placeholderTextColor="#999"
-                            style={[
-                                globalStyles.input,
-                                { paddingLeft: 50, paddingRight: 10 },
-                            ]}
-                            keyboardType="default"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            onBlur={onBlur}
-                            secureTextEntry={showPassword}
-                            onChangeText={(value) => onChange(value)}
-                            value={value}
-                        />
+                        <View style={registerForm.inputContainer}>
+                            <IconInput
+                                iconName={showPassword ? "eye-off" : "eye"}
+                                action={handleShowPassword}
+                            />
+                            <TextInput
+                                placeholder="Digite sua senha..."
+                                placeholderTextColor="#999"
+                                style={[
+                                    registerForm.input,
+                                    { paddingLeft: 50, paddingRight: 10 },
+                                ]}
+                                keyboardType="default"
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onBlur={onBlur}
+                                secureTextEntry={showPassword}
+                                onChangeText={(value) => onChange(value)}
+                                value={value}
+                            />
+                        </View>
                     )}
                 />
 
-                <InputIcon
-                    iconName={showPassword ? "eye-off" : "eye"}
-                    action={handleShowPassword}
-                />
-
                 {errors.senha && (
-                    <Text style={globalStyles.errorText}>
+                    <Text style={registerForm.errorText}>
                         {errors.senha.message}
                     </Text>
                 )}
             </View>
 
             <View style={{ marginBottom: 8 }}>
-                <Text style={globalStyles.label}>Confirmar senha</Text>
+                <Text style={registerForm.label}>Confirmar senha</Text>
                 <Controller
                     control={control}
                     name="confirmarSenha"
                     rules={{ required: true }}
                     render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Confirme sua senha..."
-                            placeholderTextColor="#999"
-                            style={[
-                                globalStyles.input,
-                                { paddingLeft: 50, paddingRight: 10 },
-                            ]}
-                            keyboardType="default"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            onBlur={onBlur}
-                            secureTextEntry={showPassword}
-                            onChangeText={(value) => onChange(value)}
-                            value={value}
-                        />
+                        <View style={registerForm.inputContainer}>
+                            <IconInput
+                                iconName={
+                                    showConfirmPassword ? "eye-off" : "eye"
+                                }
+                                action={handleShowConfirmPassword}
+                            />
+                            <TextInput
+                                placeholder="Confirme sua senha..."
+                                placeholderTextColor="#999"
+                                style={[
+                                    registerForm.input,
+                                    { paddingLeft: 50, paddingRight: 10 },
+                                ]}
+                                keyboardType="default"
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onBlur={onBlur}
+                                secureTextEntry={showPassword}
+                                onChangeText={(value) => onChange(value)}
+                                value={value}
+                            />
+                        </View>
                     )}
                 />
 
-                <InputIcon
-                    iconName={showConfirmPassword ? "eye-off" : "eye"}
-                    action={handleShowConfirmPassword}
-                />
-
                 {errors.confirmarSenha && (
-                    <Text style={globalStyles.errorText}>
+                    <Text style={registerForm.errorText}>
                         {errors.confirmarSenha.message}
                     </Text>
                 )}
             </View>
 
-            <ButtonForm
-                textButton="Cadastrar"
-                action={handleSubmit(onSubmit)}
-            />
+            <View style={{ marginTop: 24 }}>
+                <Button color={COLORS.red} onPress={handleSubmit(onSubmit)}>
+                    <Title color={COLORS.white} size={14} fontWeight="bold">
+                        <ArrowRight size={16} color={COLORS.white} />
+                        Criar minha conta
+                    </Title>
+                </Button>
+            </View>
         </View>
     );
 }
