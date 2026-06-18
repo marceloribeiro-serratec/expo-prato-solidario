91bbd0a7743f7439892a4d922151e77ac2d63334
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-18 18:35:39 -0300

    feat: refactor PageContainerScroll to remove SafeAreaView and streamline layout with ImageBackground

--- src/components/PageContainerScroll/style.ts
+++ src/components/PageContainerScroll/style.ts
@@ -2,12 +2,11 @@ import { COLORS } from '@/constants/colors';
 import { StyleSheet } from 'react-native';
 
 export const pageContainer = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLORS.white,
-    },
     backgroundImage: {
         flex: 1,
+        width: '100%',
+        height: '100%',
+        backgroundColor: COLORS.white,
     },
     scroll: {
         flex: 1,
