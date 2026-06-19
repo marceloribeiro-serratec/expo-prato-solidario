f83bfcc8b058e96d1a279aa09ade8e8771f2a388
josemello26 <jose.mello@residente.serratec.org>
2026-06-19 18:44:05 -0300

    fix:ajuste-estilo-componente-searchbar.

---
+++ src/components/SearchBar/styles.ts
@@ -0,0 +1,13 @@
+import { COLORS } from "@/constants";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    input: {
+        flex: 1,
+        fontSize: 14,
+        color: COLORS.gray_700,
+        height: '100%',
+        paddingLeft: 8,
+        textAlignVertical: 'center',
+    }
+});
