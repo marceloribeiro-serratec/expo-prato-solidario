fb659f6d985cdf2bc4cde23ec26981a4e21945cb
Emanuel Valinhos <valinhos.emanuel27@gmail.com>
2026-06-19 00:25:36 -0300

    alter: Gambiarra temporária para não quebrar o código e liberar a comunicação da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
 
+
+// Coloquei essas alterações, com export const e default, além dos dois valores na constante
+// da anon_key só para não quebrar o código. O ideal é padronizar para manter apenas um tipo
+// --Emanuel
 const base_url = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const anon_key = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const anon_key = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY; 
 
 export const api = axios.create({
     baseURL: `${base_url}/rest/v1`,
     headers: {
         "Content-Type": "application/json",
-        "apikey": anon_key,
-        "Authorization": `Bearer ${anon_key}`
+        "apikey": anon_key!,
+        "Authorization": `Bearer ${anon_key!}`
     }
 });
+
+export default api;
