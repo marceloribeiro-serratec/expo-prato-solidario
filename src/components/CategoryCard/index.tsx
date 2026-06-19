a59ca48c9105a9d94f96e8d6fee8451cfd788c95
marceloribeiro-serratec <marcelo.ribeiro@proeja.com>
2026-06-19 16:33:43 -0300

    feat: refactor CategoryCard component and update styles for improved layout

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -1,18 +1,18 @@
-import { View, Text } from "react-native";
-import { category, categoryCard } from "./style";
-import { ButtonIcon } from "../ButtonIcon";
+import { View } from "react-native";
+import { categoryCard } from "./style";
+import { Hamburger } from "lucide-react-native";
+import { COLORS } from "@/constants";
+import { Title } from "../Title";
 
 export function CategoryCard() {
     return (
-        <View style={ category.container}>
-             <View
-                style={[
-                    categoryCard.iconContainer,
-                    { backgroundColor: variantStyles.backgroundIcon },
-                ]}
-            >
-                <Icon color={variantStyles.iconColor} size={30} />
+        <View style={categoryCard.container}>
+            <View style={categoryCard.iconContainer}>
+                <Hamburger color={COLORS.green_dark} size={24} />
             </View>
+            <Title color={COLORS.black} size={24} fontWeight={400}>
+                Hamburguer
+            </Title>
         </View>
-    )
+    );
 }
--- src/components/CategoryCard/style.ts
+++ src/components/CategoryCard/style.ts
@@ -6,12 +6,19 @@ export const categoryCard = StyleSheet.create({
         backgroundColor: COLORS.gray_100,
         borderWidth: 1,
         borderColor: COLORS.gray_300,
+        borderRadius: 12,
+        padding: 12,
+        width: 80,
+        height: 80,
+        alignItems: "center",
+        justifyContent: "center",
     },
     iconContainer: {
         width: 40,
         height: 40,
         borderRadius: 20,
+        backgroundColor: COLORS.green_light,
         alignItems: "center",
         justifyContent: "center",
     },
-})
+});
