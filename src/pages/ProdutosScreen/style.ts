a2f30185f71ff61aaa765b7dfac7981cb49eb63c
João Gabriel Silva Barros <guieste2@gmail.com>
2026-06-19 18:34:48 -0300

    feat: add Header em ProdutosScreen

--- src/pages/ProdutosScreen/style.ts
+++ src/pages/ProdutosScreen/style.ts
@@ -1,11 +1,22 @@
+import { COLORS } from "@/constants";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#F5F5F5",
+        backgroundColor: COLORS.gray_100,
         paddingHorizontal: 16,
         paddingTop: 16,
+    },
+
+    headerContainer: {
+        backgroundColor: COLORS.gray_100,
+        paddingHorizontal: 12,
+        paddingVertical: 14,
+        borderBottomWidth: 1,
+        borderBottomColor: COLORS.gray_200,
+        justifyContent: "center",
+        // Isso garante que o header herde o comportamento de alinhamento
     },
 
     input: {
