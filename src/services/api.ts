9e54e9dd24a15d0c3c1754213ddba6d533395d93
Emanuel Valinhos <valinhos.emanuel27@gmail.com>
2026-06-18 23:53:22 -0300

    feat: Criando Service para Produtos e Categorias

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_API_URL,
-});
+const base_url = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const anon_key = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export default api;
+export const api = axios.create({
+    baseURL: `${base_url}/rest/v1`,
+    headers: {
+        "Content-Type": "application/json",
+        "apikey": anon_key,
+        "Authorization": `Bearer ${anon_key}`
+    }
+});
